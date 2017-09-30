a173a1ae01e0150676c0cd40d5ce50e742f38a82
Felipe Hernandez <felipeneo@hotmail.com>
2017-09-30 10:31:29 -0400

    build loop in view to show list of planets in planets view

--- planets_app/config/routes.rb
+++ planets_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # resources :pages, only: [ :index, :show ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/planets/:planet', to: 'planets'
   get '/planets', to: 'pages#planets'
   root 'pages#home'
 
